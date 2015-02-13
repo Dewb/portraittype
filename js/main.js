f0f1094035372da665a130b6963db854dffed754
Michael Dewberry <michael.dewberry@gmail>
2015-02-13 02:41:36 -0500

    Cursor position awareness, invisible asterisks, typewriter font

--- js/main.js
+++ js/main.js
@@ -1,33 +1,71 @@
 $(document).ready(function() {
 
-	var previousPoem = "";
+	var dirtyWord = undefined;
 
-	$('#poetry').bind('input propertychange', function() {
+	function getCursorPosition() {
+		var ta = $('#poetry');
+		return ta.prop("selectionDirection") == "backward" ? ta.prop("selectionStart") : ta.prop("selectionEnd");
+	}
 
+	function draw(contentChanged) {
 		var poem = $('#poetry').val();
-		//var differences = diff_main(previousPoem, poem);
+	    var cursorPosition = getCursorPosition();
       	
       	$('#displayarea').empty();
-      	var words = poem.match(/\S+|\n/g);
-      	var numWords = words.length;
-
-      	// Don't show last word if there's not a space or newline after it
-      	if (poem.slice(-1) != " ") {
-      		numWords--;
+      	var words = poem.match(/[ \t]+|\S+|\n/g);
+      	if (words == undefined) { 
+      		return;
       	}
+      	var numWords = words.length;
+      	dirty = false;
+      	var cursorInRealWord = false;
 
+      	var currentPosition = 0;
       	for (var i = 0; i < numWords; i++) {
       		var word = words[i];
+      		var cursorInWord = currentPosition <= cursorPosition && currentPosition + word.length >= cursorPosition;
+      		currentPosition += word.length;
+
       		if (word == "\n") {
 	      		$('#displayarea').append($('<br />'));	
 	      		continue;
       		}
+      		if (/^\s+$/.test(word)) {
+      			continue;
+      		}
+
+      		var drawWord = true;
+      		if (cursorInWord) {
+      			cursorInRealWord = true;
+	      		if (contentChanged) {
+	      			dirtyWord = i;
+					drawWord = false;
+	      		} else if (dirtyWord == i) {
+	      			drawWord = false;
+	      		}
+			}
+
+			word = word.replace(/\*/g, " ");
 	      	wordSpan = $('<span />').addClass('poem_word').html(word + " ");
-	      	if (word.length > 5) {
+			
+	      	if (drawWord && word.length > 5) {
 	      		wordSpan.addClass('long_word');
 	      	}
 	      	$('#displayarea').append(wordSpan);
 		}
+
+		if (!cursorInRealWord) {
+			dirtyWord = undefined;
+		}
+	}
+
+
+	$('#poetry').bind('input propertychange', function() {
+		draw(true);
 	});
+
+	$('#poetry').bind('keyup click', function() {		
+		draw(false);
+	})
 
 });
