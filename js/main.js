78991c180cce6cc66a3c5b4470faa69217577c0b
Michael Dewberry <michael.dewberry@gmail>
2015-02-13 03:46:52 -0500

    Fade in new words

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 
 	var dirtyWord = undefined;
+	var previousText = undefined;
+	var updatesEnabled = true;
 
 	function getCursorPosition() {
 		var ta = $('#poetry');
 		return ta.prop("selectionDirection") == "backward" ? ta.prop("selectionStart") : ta.prop("selectionEnd");
 	}
 
-	function draw(contentChanged) {
+	function draw() {
 		var poem = $('#poetry').val();
 	    var cursorPosition = getCursorPosition();
+	    var contentChanged = previousText != poem;
       	
       	$('#displayarea').empty();
       	var words = poem.match(/[ \t]+|\S+|\n/g);
@@ -17,9 +20,9 @@ $(document).ready(function() {
       		return;
       	}
       	var numWords = words.length;
-      	dirty = false;
       	var cursorInRealWord = false;
 
+		var lastDirtyWord = dirtyWord;
       	var currentPosition = 0;
       	for (var i = 0; i < numWords; i++) {
       		var word = words[i];
@@ -35,6 +38,7 @@ $(document).ready(function() {
       		}
 
       		var drawWord = true;
+      		var fadeWord = false;
       		if (cursorInWord) {
       			cursorInRealWord = true;
 	      		if (contentChanged) {
@@ -43,29 +47,46 @@ $(document).ready(function() {
 	      		} else if (dirtyWord == i) {
 	      			drawWord = false;
 	      		}
+			} else if (lastDirtyWord == i) {
+				fadeWord = true;
 			}
 
 			word = word.replace(/\*/g, " ");
 	      	wordSpan = $('<span />').addClass('poem_word').html(word + " ");
 			
-	      	if (drawWord && word.length > 5) {
-	      		wordSpan.addClass('long_word');
+	      	if (word.length < 6) {
+	      		drawWord = false;
+	      		fadeWord = false;
 	      	}
+
+	      	if (drawWord && fadeWord) {
+	      		wordSpan.hide();
+			} else if (!drawWord) {
+				wordSpan.css('opacity', 0);
+			}
+
 	      	$('#displayarea').append(wordSpan);
+	      	
+	      	if (fadeWord && drawWord) {
+	      		updatesEnabled = false;
+	      		wordSpan.fadeIn(800, function () {
+	      			updatesEnabled = true;
+	      			lastDirtyWord = undefined;
+	      			draw();
+	      		});
+	      	}
 		}
 
 		if (!cursorInRealWord) {
 			dirtyWord = undefined;
 		}
+		previousText = poem;
 	}
 
-
-	$('#poetry').bind('input propertychange', function() {
-		draw(true);
+	$('#poetry').bind('keyup click', function() {	
+		if (updatesEnabled) {
+			draw();
+		}
 	});
-
-	$('#poetry').bind('keyup click', function() {		
-		draw(false);
-	})
 
 });
